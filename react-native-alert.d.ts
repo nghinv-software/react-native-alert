70557efc99b6a602f7a80c57904c6341f752213d
NghiNV <nghinv@lumi.biz>
2021-02-17 10:33:32 +0700

    add typing

---
+++ react-native-alert.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by nghinv on Wed Feb 17 2021
+ * Copyright (c) 2021 nghinv@lumi.biz
+ */
+
+declare module 'react-native-alert' {
+  import { PureComponent } from 'react';
+  import { ViewStyle } from 'react-native';
+
+  interface AlertServiceProps {
+
+  }
+
+  export class AlertService extends PureComponent<AlertServiceProps> {
+
+  }
+}
